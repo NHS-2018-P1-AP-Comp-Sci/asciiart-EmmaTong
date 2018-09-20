ee807ccf98a93eb4ac0c131242e45498e67638f8
s-tongc <s-tongc@53S500038070.it.bsd405.org>
2018-09-20 08:00:40 -0700

    This is the first project code.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -10,8 +10,101 @@
  * For those of you who have dabbled in coding before, try to contain your code to 
  * just what we have discussed in this unit.  
  * 
- * @author Mr. Rasmussen 
+ * @author Mr. Rasmussen        
  *
  */
 
 // YOUR CODE GOES HERE :)
+/*
+ * 		   	_         _
+ *         (c)  .-.  (c)	
+ *          /   ._.   \
+ *        __\ (  Y  ) /__
+ *       (__.- /'-'\ -.__)
+ *          | |  X  | |
+ *        _.'  ` - '  '._
+ *       (.-./ ` - ' \.-.)
+ *        `-'         `-'
+ *        
+ *  <  I am the teddy bear. >
+ * 
+ */
+public class ASCIIArt {
+	public static void main(String[] args) {
+		Head();
+		UpperBody();
+		LowerBody();
+		System.out.println("\n\n< I am the teddy bear. >");
+	}
+	public static void Ear() {
+	
+		System.out.print("(c)");
+	}
+	public static void DownwardEyes() {
+		System.out.print(".-.");
+	}
+	public static void Eyes() {
+		System.out.print("._.");
+	}
+	public static void Nose() {
+		System.out.print("(  Y  )");
+	}
+	public static void Neck() {
+		System.out.print("/'-'\\");
+	}
+	public static void Stomache() {
+		System.out.println("| |  X  | |");
+	}
+	public static void Leg() {
+		System.out.print("` - '");
+	}
+	public static void Foot() {
+		System.out.print(" `-'");
+	}
+	public static void Head() {
+		System.out.print("   _         _\n  ");
+		Ear();
+		System.out.print("  .-.  ");
+		Ear();
+		System.out.print("\n   /   ");
+		Eyes();
+		System.out.print("   \\  \n __\\ ");
+		Nose();
+		System.out.print(" /__");
+	}
+	public static void UpperBody() {
+		System.out.print("\n(__.- ");
+		Neck();
+		System.out.println(" -.__)");
+		System.out.print("   ");
+		Stomache();
+
+	}
+	public static void LowerBody() {
+		System.out.print(" _.'  ");
+		Leg();
+		System.out.println("  '._");
+		System.out.print("(");
+		DownwardEyes();
+		System.out.print("/ ");
+		Leg();
+		System.out.print(" \\");
+		DownwardEyes();
+		System.out.println(")");
+		Foot();
+		System.out.print("        ");
+		Foot();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
